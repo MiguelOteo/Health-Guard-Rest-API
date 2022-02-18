49df47e17fb238f463c2dd39dd9572de915e9857
Miguel Oteo Marín <47748362+MiguelOteo@users.noreply.github.com>
2022-02-18 19:14:44 +0100

    Update config.php

--- src/main/php/config.php
+++ src/main/php/config.php
@@ -4,7 +4,7 @@
 	
 	define("DB_HOST", "localhost");
 	define("DB_USER", "root");
-	define("DB_PASSWORD", "mi1guel23");
+	define("DB_PASSWORD", "your password");
 	define("DB_DATABASE", "healthguard");
 
 ?>
